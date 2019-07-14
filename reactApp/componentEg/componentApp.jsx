389b838350c4a9f97315c2843026f9ba5451bd09
inest-us <hieung@live.com>
2019-07-13 20:50:28 -0700

    component APIs and Life Cycle

--- reactApp/componentEg/componentApp.jsx
+++ reactApp/componentEg/componentApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
    constructor(props) {
@@ -24,8 +25,34 @@ class App extends React.Component {
                "name":"Baz",
                "age":"40"
             }
-         ]
-      }
+         ],
+         data2: [],
+         data3: 0
+      };
+      this.setStateHandler = this.setStateHandler.bind(this);
+      this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+      this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+      this.setNewNumber = this.setNewNumber.bind(this);
+   }
+
+   setStateHandler() {
+      var item = 'setState...';
+      var myArray = this.state.data2.slice();
+      myArray.push(item);
+      this.setState({data2: myArray});
+   }
+
+   forceUpdateHandler() {
+      this.forceUpdate();
+   }
+
+   findDomNodeHandler() {
+      var myDiv = document.getElementById('myDiv');
+      ReactDOM.findDOMNode(myDiv).style.color = 'green';
+   }
+
+   setNewNumber() {
+      this.setState({data3 : this.state.data3 + 1})
    }
    render() {
       return (
@@ -33,6 +60,22 @@ class App extends React.Component {
             <Header headerProp={this.state.header} />
             <h3>Array: {this.props.propArray}</h3>
             <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
+            <div>
+               <button onClick = {this.setStateHandler}>SET STATE</button>
+               <h4>State Array: {this.state.data2}</h4>
+            </div>
+            <div>
+               <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
+               <h4>Random number: {Math.random()}</h4>
+            </div>
+            <div>
+            <button onClick = {this.findDomNodeHandler}>FIND DOME NODE</button>
+               <div id = "myDiv">NODE</div>
+            </div>
+            <div>
+               <button onClick = {this.setNewNumber}>INCREMENT</button>
+               <Content myNumber = {this.state.data3}></Content>
+            </div>
             <table>
                <tbody>
                   {this.state.data.map((person, i) => <TableRow key = {i} 
@@ -52,6 +95,38 @@ class Header extends React.Component {
       );
    }
 }
+
+class Content extends React.Component {
+   componentWillMount() {
+      console.log('Component WILL MOUNT!')
+   }
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
+   }
+   componentWillReceiveProps(newProps) {    
+      console.log('Component WILL RECIEVE PROPS!')
+   }
+   shouldComponentUpdate(newProps, newState) {
+      return true;
+   }
+   componentWillUpdate(nextProps, nextState) {
+      console.log('Component WILL UPDATE!');
+   }
+   componentDidUpdate(prevProps, prevState) {
+      console.log('Component DID UPDATE!')
+   }
+   componentWillUnmount() {
+      console.log('Component WILL UNMOUNT!')
+   }
+   render() {
+      return (
+         <div>
+            <h3>{this.props.myNumber}</h3>
+         </div>
+      );
+   }
+}
+
 class TableRow extends React.Component {
    render() {
       return (
